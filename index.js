c41e0e19eb538ccbb9a20e0f0f5232f34baae693
Jay Sitter <jay.sitter@dockyard.com>
2017-12-08 12:44:54 -0500

    Add stylelint-order plugin and SMACSS property sort order with DockYard overrides, using DockYard styleguide: https://github.com/dockyard/styleguides/blob/master/ux-dev/css.md

--- index.js
+++ index.js
@@ -3,9 +3,59 @@
 module.exports = {
   "extends": "stylelint-config-standard",
   "plugins": [
-    "stylelint-csstree-validator"
+    "stylelint-csstree-validator",
+    "stylelint-order"
   ],
   "rules": {
+    "order/order": [
+      "custom-properties",
+      "declarations"
+    ],
+    "order/properties-order": [
+      { "properties": [ "content", "quotes" ] },
+
+      { "properties": [ "margin", "margin-top", "margin-right", "margin-bottom", "margin-left" ] },
+      { "properties": [ "padding", "padding-top", "padding-right", "padding-bottom", "padding-left" ] },
+
+      { "properties": [ "box-sizing" ] },
+      { "properties": [ "flex", "flex-basis", "flex-direction", "flex-flow", "flex-grow", "flex-shrink", "flex-wrap", "align-content", "align-items", "align-self", "justify-content", "order" ] },
+      { "properties": [ "grid-template-columns", "grid-template-rows", "grid-template-areas", "grid-template", "grid-auto-columns", "grid-auto-rows", "grid-auto-flow", "grid", "grid-row-start", "grid-column-start", "grid-row-end", "grid-column-end", "grid-row", "grid-column", "grid-area", "grid-row-gap", "grid-column-gap", "grid-gap" ] },
+      { "properties": [ "width", "min-width", "max-width", "height", "min-height", "max-height" ] },
+      { "properties": [ "display", "visibility" ] },
+      { "properties": [ "float", "clear" ] },
+      { "properties": [ "overflow", "overflow-x", "overflow-y" ] },
+      { "properties": [ "clip", "zoom" ] },
+      { "properties": [ "columns", "column-gap", "column-fill", "column-rule", "column-span", "column-count", "column-width" ] },
+      { "properties": [ "table-layout", "empty-cells", "caption-side", "border-spacing", "border-collapse", "list-style", "list-style-position", "list-style-type", "list-style-image" ] },
+      { "properties": [ "position", "z-index", "top", "right", "bottom", "left" ] },
+
+      { "properties": [ "font", "font-family", "font-size", "font-size-adjust", "font-stretch", "font-effect", "font-style", "font-variant", "font-weight" ] },
+      { "properties": [ "font-emphasize", "font-emphasize-position", "font-emphasize-style" ] },
+      { "properties": [ "color" ] },
+      { "properties": [ "line-height", "list-style", "word-spacing" ] },
+      { "properties": [ "letter-spacing" ] },
+      { "properties": [ "text-align", "text-align-last", "text-decoration", "text-indent", "text-justify", "text-overflow", "text-overflow-ellipsis", "text-overflow-mode", "text-rendering", "text-outline", "text-shadow", "text-transform", "text-wrap", "word-wrap", "word-break" ] },
+      { "properties": [ "text-emphasis", "text-emphasis-color", "text-emphasis-style", "text-emphasis-position" ] },
+      { "properties": [ "vertical-align", "white-space", "word-spacing", "hyphens" ] },
+      { "properties": [ "src" ] },
+
+      { "properties": [ "border", "border-top", "border-right", "border-bottom", "border-left", "border-width", "border-top-width", "border-right-width", "border-bottom-width", "border-left-width" ] },
+      { "properties": [ "border-style", "border-top-style", "border-right-style", "border-bottom-style", "border-left-style" ] },
+      { "properties": [ "border-color", "border-top-color", "border-right-color", "border-bottom-color", "border-left-color" ] },
+      { "properties": [ "border-radius", "border-top-left-radius", "border-top-right-radius", "border-bottom-left-radius", "border-bottom-right-radius" ] },
+      { "properties": [ "outline", "outline-color", "outline-offset", "outline-style", "outline-width" ] },
+      { "properties": [ "stroke-width", "stroke-linecap", "stroke-dasharray", "stroke-dashoffset", "stroke" ] },
+
+      { "properties": [ "background", "background-color", "background-image", "background-repeat", "background-position", "background-size", "box-shadow", "fill" ] },
+      { "properties": [ "opacity" ] },
+
+      { "properties": [ "transition", "transition-property", "transition-duration", "transition-timing-function", "transition-delay" ] },
+
+      { "properties": [ "transform", "transform-origin", "transform-style", "backface-visibility", "perspective", "perspective-origin" ] },
+      { "properties": [ "animation", "animation-name", "animation-duration", "animation-play-state", "animation-timing-function", "animation-delay", "animation-iteration-count", "animation-direction" ] },
+
+      { "properties": [ "tab-size", "counter-reset", "counter-increment", "resize", "cursor", "pointer-events", "speak", "user-select", "nav-index", "nav-up", "nav-right", "nav-down", "nav-left" ] }
+    ],
     "at-rule-empty-line-before": null,
     "at-rule-name-space-after": "always",
     "at-rule-no-unknown": true,
